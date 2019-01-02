2955031421b20244d6984cc35d8ad51f4fb67355
Levi Zitting <levizitting5663@gmail.com>
2019-01-01 20:40:40 -0600

    Remove carousel code

--- assets/js/components/AppCarousel.js
+++ assets/js/components/AppCarousel.js
@@ -1,39 +1,3 @@
 export default {
-    props: {
-        totalSlides: String,
-    },
-    data: () => ({
-        active: 0,
-        animation: '',
-    }),
-    render() {
-        return this.$scopedSlots.default({
-            active: this.active,
-            next: this.next,
-            previous: this.previous,
-            animation: this.animation,
-        });
-    },
-    methods: {
-        next() {
-            this.animation = 'next-slide';
 
-            if (this.active >= this.totalSlides - 1) {
-                this.active = 0;
-                return;
-            }
-
-            this.active++;
-        },
-        previous() {
-            this.animation = 'prev-slide';
-
-            if(this.active <= 0) {
-                this.active = this.totalSlides - 1;
-                return;
-            }
-
-            this.active--;
-        }
-    }
 };
